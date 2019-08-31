de7e8ee573c36c0f7474f848a72e317534ff30d3
Ajit Johnson Nirmal <ajitjohnson.n@gmail.com>
2019-08-31 15:56:21 -0400

    dplyr import fix

--- R/arseq.R
+++ R/arseq.R
@@ -11,7 +11,7 @@
 #' @import GenomeInfoDbData
 #' @import goseq
 #' @import plyr
-#' @importFrom dplyr tbl_df "%>%"
+#' @importFrom dplyr tbl_df filter "%>%"
 #' @import ggplot2
 #' @import gridExtra
 #' @import stringr
